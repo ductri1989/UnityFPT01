b5fb1ff1e8405b3522f96b91a66e5edc34632944
Richard Kennedy <127181473+ductri1989@users.noreply.github.com>
2023-08-04 18:46:42 +0700

    Setup Project

---
+++ Assets/Scenes/SC_Teacher/T_Myplayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_Myplayer : MonoBehaviour{
+    [SerializeField] float jump;
+    [SerializeField] float turningSpeed;
+    [SerializeField] float movementSpeed;
+
+    private bool canJump;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        canJump = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        canJump = false;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+            rb.AddForce(Vector3.up*jump);
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 moveVector = new Vector2(horizontal, vertical);
+        GetComponent<Animator>().SetFloat("Moving", moveVector.magnitude);//magnitude : lấy độ dài của vector
+
+        //transform.Translate(horizontal * turningSpeed * Time.deltaTime, 0, vertical * movementSpeed * Time.deltaTime);//Di chuyển nhân vật
+        
+
+
+
+
+        if (Input.GetMouseButtonDown(0))
+            GetComponent<Animator>().Play("Attacking");
+
+    }
+}
