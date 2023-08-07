cfe550729637814f2172066bdaa1f238da8adeb0
Richard Kennedy <127181473+ductri1989@users.noreply.github.com>
2023-08-07 19:28:53 +0700

    Di chuyển nhân vật và camera

--- Assets/Scenes/SC_Teacher/T_Myplayer.cs
+++ Assets/Scenes/SC_Teacher/T_Myplayer.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class T_Myplayer : MonoBehaviour{
+    [SerializeField] Transform camPlayer;
     [SerializeField] float jump;
     [SerializeField] float turningSpeed;
     [SerializeField] float movementSpeed;
-
+    [SerializeField] bool isControlPlayer;
+    
     private bool canJump;
     private Rigidbody rb;
     void Start()
     {
+        isControlPlayer = true;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -22,24 +25,48 @@ public class T_Myplayer : MonoBehaviour{
         canJump = false;
     }
 
-    // Update is called once per frame
     void Update(){
+        if (isControlPlayer == false)
+            return;
+
         if (Input.GetKeyDown(KeyCode.Space) && canJump)
-            rb.AddForce(Vector3.up*jump);
-
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector2 moveVector = new Vector2(horizontal, vertical);
-        GetComponent<Animator>().SetFloat("Moving", moveVector.magnitude);//magnitude : lấy độ dài của vector
-
-        if(rb.velocity.x<300 && rb.velocity.z < 300)
-            rb.AddForce(horizontal * movementSpeed * Time.deltaTime, 0, vertical * movementSpeed * Time.deltaTime);//Di chuyển nhân vật
+            rb.AddForce(Vector3.up * jump);
 
         if (Input.GetMouseButtonDown(0))
             GetComponent<Animator>().Play("DrawArrow");
 
         if (Input.GetMouseButtonUp(0))
             GetComponent<Animator>().Play("Recoil");
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 moveVector = new Vector2(horizontal, vertical);
+        GetComponent<Animator>().SetFloat("Moving", moveVector.magnitude);//magnitude : lấy độ dài của vector
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        if (Input.GetKey(KeyCode.W) && rb.velocity.z < movementSpeed)
+            rb.AddForce(transform.forward * movementSpeed);
+        if (Input.GetKey(KeyCode.S) && rb.velocity.z > -movementSpeed)
+            rb.AddForce(-transform.forward * movementSpeed);
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+            rb.velocity = new Vector3(0, rb.velocity.y, rb.velocity.z);
+
+        if (Input.GetKey(KeyCode.D) && rb.velocity.x < movementSpeed)
+            rb.AddForce(transform.right * movementSpeed);
+        if (Input.GetKey(KeyCode.A) && rb.velocity.x > -movementSpeed)
+            rb.AddForce(-transform.right * movementSpeed);
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
+
+        float camHorizontal = Input.GetAxis("Mouse X") * turningSpeed * Time.deltaTime;
+        transform.Rotate(0, camHorizontal, 0);//Xoay nhân vật theo con chuột
+
+        float camVertical = Input.GetAxis("Mouse Y") * turningSpeed * Time.deltaTime;
+        Camera.main.transform.position = camPlayer.position;
+        Camera.main.transform.localEulerAngles = new Vector3(Camera.main.transform.localEulerAngles.x, transform.localEulerAngles.y, 0);//Xoay Y của Camera
+        Camera.main.transform.Rotate(-camVertical, 0, 0);//Xoay X của camera
     }
+
+
 }
